b889906ba2d1028fd5f9938397dc99f620fc7271
Adrian Reyer <are@lihas.de>
2008-07-10 08:41:45 +0000

    recovering from RCS unaware modification

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 ### BEGIN INIT INFO
 # Provides:          firewall
 # Required-Start:    $local_fs
@@ -35,13 +37,12 @@ SCRIPTNAME=/etc/init.d/$NAME
 . /lib/lsb/init-functions
 
 do_reload() {
-  	do_start
-	return 0
+        do_start
+        return 0
 }
 
-
-
 do_start() {
+
 ipt_cmd=0
 ipt_err=0
 
@@ -97,18 +98,47 @@ for iface in interface-*; do
   echo "-A POSTROUTING -o $iface -j post-$iface" >> $FILEnat
 done
 
+echo "Loopback Interface is fine"
+echo "-A OUTPUT	-j ACCEPT -o lo" >> $FILEfilter
+echo "-A INPUT	-j ACCEPT -i lo" >> $FILEfilter
+
 echo "Adding DNAT"
+ipt_lihas_dnat () {
+  outfile=$1
+  dnet=$2
+  mnet=$3
+  proto=$4
+  dport=$5
+  ndport=$6
+  if [ $dnet == "include" ]; then
+    if [ -e $mnet ]; then
+      cat $mnet | sed '/^[ \t]*$/d; /^#/d' |
+      while read dnet mnet proto dport ndport; do
+        ipt_lihas_dnat "$outfile" "$dnet" "$mnet" "$proto" "$dport" "$ndport"
+      done
+    else
+      echo "$mnet doesn't exist"
+    fi
+  else
+    if [ $dport == "0" ]; then
+      echo "-A pre-$iface -d $dnet -p $proto --to-destination $mnet" >> $FILEnat
+    else
+      if [ $proto == "icmp" ]; then
+        echo "-A pre-$iface -d $dnet -p $proto --icmp-type $dport -j DNAT --to-destination $mnet:$ndport" >> $FILEnat
+      else 
+        echo "-A pre-$iface -d $dnet -p $proto --dport $dport -j DNAT --to-destination $mnet:$ndport" >> $FILEnat
+      fi
+    fi
+  fi
+}
+
 for iface in interface-*; do
   iface=${iface#interface-}
   [ -e interface-$iface/comment ] && cat interface-$iface/comment | sed 's/^/ /'
   if [ -e interface-$iface/dnat ]; then
     cat interface-$iface/dnat | sed '/^[ \t]*$/d; /^#/d' |
     while read dnet mnet proto dport ndport; do
-      if [ $dport == "0" ]; then
-        echo "-A pre-$iface -d $dnet -p $proto --to-destination $mnet" >> $FILEnat
-      else
-        echo "-A pre-$iface -d $dnet -p $proto --dport $dport -j DNAT --to-destination $mnet:$ndport" >> $FILEnat
-      fi
+      ipt_lihas_dnat "$FILEnat" "$dnet" "$mnet" "$proto" "$dport" "$ndport"
     done
   fi
 done
@@ -123,7 +153,11 @@ for iface in interface-*; do
       if [ $dport == "0" ]; then
         echo "-A post-$iface -s $snet -p $proto -j SNAT --to-source $mnet" >> $FILEnat
       else
-        echo "-A post-$iface -s $snet -p $proto --dport $dport -j SNAT --to-source $mnet" >> $FILEnat
+        if [ $proto == "icmp" ]; then
+          echo "-A post-$iface -s $snet -p $proto --icmp-type  $dport -j SNAT --to-source $mnet" >> $FILEnat
+        else 
+          echo "-A post-$iface -s $snet -p $proto --dport $dport -j SNAT --to-source $mnet" >> $FILEnat
+	fi
       fi
     done
   fi
@@ -139,7 +173,11 @@ for iface in interface-*; do
       if [ $dport == "0" ]; then
         echo "-A post-$iface -s $snet -p $proto -j MASQUERADE" >> $FILEnat
       else
-        echo "-A post-$iface -s $snet -p $proto --dport $dport -j MASQUERADE" >> $FILEnat
+        if [ $proto == "icmp" ]; then
+          echo "-A post-$iface -s $snet -p $proto --icmp-type  $dport -j MASQUERADE" >> $FILEnat
+        else 
+          echo "-A post-$iface -s $snet -p $proto --dport $dport -j MASQUERADE" >> $FILEnat
+        fi
       fi
     done
   fi
@@ -161,10 +199,19 @@ for iface in interface-*; do
         fi
       else
         if [ "ga$oiface" == "ga" ]; then
-          echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -j ACCEPT" >> $FILEfilter
-          echo "-A in-$iface -m state --state new -s $snet -d $dnet -p $proto -j ACCEPT" >> $FILEfilter
+          if [ $proto == "icmp" ]; then
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --icmp-type $dport -j ACCEPT" >> $FILEfilter
+            echo "-A in-$iface -m state --state new -s $snet -d $dnet -p $proto --icmp-type $dport -j ACCEPT" >> $FILEfilter
+          else 
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -j ACCEPT" >> $FILEfilter
+            echo "-A in-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -j ACCEPT" >> $FILEfilter
+          fi
         else
-          echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -o $oiface -j ACCEPT" >> $FILEfilter
+          if [ $proto == "icmp" ]; then
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --icmp-type $dport -o $oiface -j ACCEPT" >> $FILEfilter
+          else 
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -o $oiface -j ACCEPT" >> $FILEfilter
+          fi
         fi
       fi
     done
@@ -174,7 +221,8 @@ done
 echo LOCALHOST
 . ./localhost
 
-echo Disable specific logging
+# Disable specific logging
+echo "Disable some logging"
 for iface in interface-*; do
   iface=${iface#interface-}
   [ -e interface-$iface/comment ] && cat interface-$iface/comment | sed 's/^/ /'
@@ -190,8 +238,14 @@ for iface in interface-*; do
         fi
       else
         if [ "ga$oiface" == "ga" ]; then
-          echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -j DROP" >> $FILEfilter
-          echo "-A in-$iface -m state --state new -s $snet -d $dnet -p $proto -j DROP" >> $FILEfilter
+          if [ $proto == "icmp" ]; then
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --icmp-type $dport -o $oiface -j ACCEPT" >> $FILEfilter
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --icmp-type $dport -j DROP" >> $FILEfilter
+            echo "-A in-$iface -m state --state new -s $snet -d $dnet -p $proto --icmp-type $dport -j DROP" >> $FILEfilter
+          else 
+            echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -j DROP" >> $FILEfilter
+            echo "-A in-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -j DROP" >> $FILEfilter
+	  fi
         else
           echo "-A fwd-$iface -m state --state new -s $snet -d $dnet -p $proto --dport $dport -o $oiface -j DROP" >> $FILEfilter
         fi
@@ -205,52 +259,6 @@ for chain in INPUT OUTPUT FORWARD; do
 done
 
 
-echo Policy Routing
-echo "-I PREROUTING -j MARK --set-mark 0" >> $FILEmangle
-echo "-I OUTPUT -j MARK --set-mark 0" >> $FILEmangle
-for policy in policy-routing-*; do
-  policy=${policy#policy-routing-}
-  [ -e policy-routing-$policy/comment ] && cat policy-routing-$policy/comment | sed 's/^/ /'
-  key=$(cat policy-routing-$policy/key)
-  if [ -e policy-routing-$policy/gateway ]; then
-    cat policy-routing-$policy/gateway | sed '/^[ \t]*$/d; /^#/d' |
-    while read type interface; do
-      if [ $type == "PPP" ]; then
-        ip route flush table $policy
-	ip route ls |
-	sed 's/^default.*/default dev '$interface'/' |
-	while read a; do
-	  ip route add $a table $policy
-	done
-	while ip rule | grep 'lookup '$policy; do
-	  ip rule del fwmark $key table $policy
-	done
-	ip rule add fwmark $key table $policy
-	ip route flush cache
-      else
-        echo Non PPP-Policy-Routing is not implemented
-      fi
-    done
-  fi
-done
-for iface in interface-*; do
-  iface=${iface#interface-}
-  [ -e interface-$iface/comment ] && cat interface-$iface/comment | sed 's/^/ /'
-  if [ -e interface-$iface/policy-routing ]; then
-    cat interface-$iface/policy-routing | sed '/^[ \t]*$/d; /^#/d' |
-    while read snet dnet proto dport policy; do
-      mark=$(cat policy-routing-$policy/key)
-      if [ $dport == "0" ]; then
-          echo "-A OUTPUT -s $snet -d $dnet -p $proto -j MARK --set-mark $mark" >> $FILEmangle
-          echo "-A PREROUTING -s $snet -d $dnet -p $proto -j MARK --set-mark $mark" >> $FILEmangle
-      else
-          echo "-A OUTPUT -s $snet -d $dnet -p $proto --dport $dport -j MARK --set-mark $mark" >> $FILEmangle
-          echo "-A PREROUTING -s $snet -d $dnet -p $proto --dport $dport -j MARK --set-mark $mark" >> $FILEmangle
-      fi
-    done
-  fi
-done
-
 echo *filter > $FILE
 cat $FILEfilter >> $FILE
 echo COMMIT >> $FILE
@@ -262,68 +270,65 @@ cat $FILEnat >> $FILE
 echo COMMIT >> $FILE
 
 iptables-restore < $FILE
+
 }
 
 do_stop () {
   iptables-restore < /etc/firewall.lihas.d/iptables-accept
 }
 
-
-
-
 case "$1" in
   start)
-	[ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
-	do_start
-	case "$?" in
-		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-	esac
-	;;
+        [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
+        do_start
+        case "$?" in
+                0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+                2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+        esac
+        ;;
   stop)
-	[ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
-	do_stop
-	case "$?" in
-		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-	esac
-	;;
+        [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
+        do_stop
+        case "$?" in
+                0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+                2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+        esac
+        ;;
   reload|force-reload)
-	#
-	# If do_reload() is not implemented then leave this commented out
-	# and leave 'force-reload' as an alias for 'restart'.
-	#
-	log_daemon_msg "Reloading $DESC" "$NAME"
-	do_start
-	log_end_msg $?
-	;;
+        #
+        # If do_reload() is not implemented then leave this commented out
+        # and leave 'force-reload' as an alias for 'restart'.
+        #
+        log_daemon_msg "Reloading $DESC" "$NAME"
+        do_start
+        log_end_msg $?
+        ;;
   restart|force-reload)
-	#
-	# If the "reload" option is implemented then remove the
-	# 'force-reload' alias
-	#
-	log_daemon_msg "Restarting $DESC" "$NAME"
-	do_stop
-	case "$?" in
-	  0|1)
-		do_start
-		case "$?" in
-			0) log_end_msg 0 ;;
-			1) log_end_msg 1 ;; # Old process is still running
-			*) log_end_msg 1 ;; # Failed to start
-		esac
-		;;
-	  *)
-	  	# Failed to stop
-		log_end_msg 1
-		;;
-	esac
-	;;
+        #
+        # If the "reload" option is implemented then remove the
+        # 'force-reload' alias
+        #
+        log_daemon_msg "Restarting $DESC" "$NAME"
+        do_stop
+        case "$?" in
+          0|1)
+                do_start
+                case "$?" in
+                        0) log_end_msg 0 ;;
+                        1) log_end_msg 1 ;; # Old process is still running
+                        *) log_end_msg 1 ;; # Failed to start
+                esac
+                ;;
+          *)
+                # Failed to stop
+                log_end_msg 1
+                ;;
+        esac
+        ;;
   *)
-	#echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
-	echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
-	exit 3
-	;;
+        #echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
+        echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
+        exit 3
+        ;;
 esac
-
 
