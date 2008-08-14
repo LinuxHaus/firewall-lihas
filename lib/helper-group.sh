61dcfcd49127bc7f44bc80ff967419c63d8a4380
Adrian Reyer <are@lihas.de>
2008-08-14 12:26:06 +0000

    *** empty log message ***

--- lib/helper-group.sh
+++ lib/helper-group.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 helper_hostgroup () {
-
   cat | while read replacement; do
     if echo $replacement | grep '\b'hostgroup- > /dev/null; then
       hostgroup_replace=$(echo $replacement | sed 's/.*hostgroup-/hostgroup-/; s/hostgroup-\([^ \t]*\)[ \t].*/hostgroup-\1/')
@@ -21,9 +20,7 @@ helper_hostgroup () {
 }
 
 helper_portgroup () {
-
   cat | while read replacement; do
-
     if echo $replacement | grep '\b'portgroup- > /dev/null; then
       repproto=$(echo $replacement | awk '{print $3}')
       portgroup_replace=$(echo $replacement | sed 's/.*portgroup-/portgroup-/; s/portgroup-\([^ \t]*\)[ \t].*/portgroup-\1/')
