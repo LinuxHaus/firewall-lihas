3174c291e7384e393c113e2083d1c86facf43278
Adrian Reyer <are@lihas.de>
2008-08-14 13:25:08 +0000

    *** empty log message ***

--- lib/helper-dns.sh
+++ lib/helper-dns.sh
@@ -2,23 +2,20 @@
 
 helper_dns () {
 
-  TMPFILE=$(mktemp -p /dev/shm firewall-helper-dns-$(date -I).XXXXXXXXXX)
-  cat > $TMPFILE
-  replacement=$(cat $TMPFILE)
-
-  if echo $replacement | grep '\b'dns- > /dev/null; then
-    dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
-    hostname=${dns_replace#dns-}
-    host -t a $hostname | while read name dummy ip; do
-      replacement=$( sed s/$dns_replace/$ip/ < $TMPFILE )
-      if echo $replacement | grep '\b'dns- > /dev/null; then
-        echo $replacement | helper_dns
-      else
-        echo $replacement
-      fi
-    done
-  else
-    echo $replacement
-  fi
-  rm $TMPFILE
+  cat | while read replacement; do
+    if echo $replacement | grep '\b'dns- > /dev/null; then
+      dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
+      hostname=${dns_replace#dns-}
+      host -t a $hostname | while read name dummy ip; do
+        repl=$( echo $replacement | sed s/$dns_replace/$ip/ )
+        if echo $repl | grep '\b'dns- > /dev/null; then
+          echo $repl | helper_dns
+        else
+          echo $repl
+        fi
+      done
+    else
+      echo $replacement
+    fi
+  done
 }
