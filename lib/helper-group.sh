4f5fb398328e4c4363d55fbc25be0556f51a699c
Adrian Reyer <are@lihas.de>
2008-08-14 11:24:08 +0000

    recovering from RCS unaware modification

--- lib/helper-group.sh
+++ lib/helper-group.sh
@@ -1,22 +1,59 @@
 #!/bin/bash
 
-helper_dns () {
+helper_hostgroup () {
 
-  TMPFILE=$(mktemp -p /dev/shm firewall-helper-dns-$(date -I).XXXXXXXXXX)
+  TMPFILE=$(mktemp -p /dev/shm firewall-helper-hostgroup-$(date -I).XXXXXXXXXX)
   cat > $TMPFILE
   replacement=$(cat $TMPFILE)
 
-  if echo $replacement | grep '\b'dns- > /dev/null; then
-    dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
-    hostname=${dns_replace#dns-}
-    host -t a $hostname | while read name dummy ip; do
-      replacement=$( sed s/$dns_replace/$ip/ < $TMPFILE )
-      if echo $replacement | grep '\b'dns- > /dev/null; then
-        echo $replacement | helper_dns
+  if echo $replacement | grep '\b'hostgroup- > /dev/null; then
+    hostgroup_replace=$(echo $replacement | sed 's/.*hostgroup-/hostgroup-/; s/hostgroup-\([^ \t]*\)[ \t].*/hostgroup-\1/')
+    cat groups/$hostgroup_replace | sed '/^[ \t]*$/d; /^#/d' |
+    while read name; do
+      replacement=$( sed s/$hostgroup_replace/$name/ < $TMPFILE )
+      if echo $replacement | grep '\b'hostgroup- > /dev/null; then
+        echo $replacement | helper_hostgroup
       else
         echo $replacement
       fi
     done
+  else
+    echo $replacement
+  fi
+  rm $TMPFILE
+}
+
+
+helper_portgroup () {
+
+  TMPFILE=$(mktemp -p /dev/shm firewall-helper-portgroup-$(date -I).XXXXXXXXXX)
+  cat > $TMPFILE
+  replacement=$(cat $TMPFILE)
+
+  if echo $replacement | grep '\b'portgroup- > /dev/null; then
+    repproto=$(echo $replacement | awk '{print $3}')
+    portgroup_replace=$(echo $replacement | sed 's/.*portgroup-/portgroup-/; s/portgroup-\([^ \t]*\)[ \t].*/portgroup-\1/')
+    if [ "$repproto" == "any" ]; then
+      cat groups/$portgroup_replace | sed '/^[ \t]*$/d; /^#/d' |
+      while read proto port; do
+        replacement=$( sed "s/$portgroup_replace/$port/g; s/any/$proto/" < $TMPFILE )
+        if echo $replacement | grep '\b'portgroup- > /dev/null; then
+          echo $replacement | helper_portgroup
+        else
+          echo $replacement
+        fi
+      done
+    else
+      cat groups/$portgroup_replace | sed '/^[ \t]*$/d; /^#/d' | awk '$1 ~ /^'$repproto'/' |
+      while read proto port; do
+        replacement=$( sed "s/$portgroup_replace/$port/g;" < $TMPFILE )
+        if echo $replacement | grep '\b'portgroup- > /dev/null; then
+          echo $replacement | helper_portgroup
+        else
+          echo $replacement
+        fi
+      done
+    fi
   else
     echo $replacement
   fi
