7a9144cd663ad1dc67089c3c3f314c5afa649c70
Adrian Reyer <are@lihas.de>
2008-08-14 09:13:36 +0000

    recovering from RCS unaware modification

--- lib/helper-group.sh
+++ lib/helper-group.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 helper_dns () {
-  dns_replace=$1
-  hostname=${$dns_replace#dns-}
-  host -t a $hostname | while read name dummy ip; do
-    replacement=$( | sed 's/'$dns_replace'/'$ip'/')
-    if echo $replacement | grep '\b'$dns_replace > /dev/null; then
-      echo replacement | helper_dns $dns_replace
-    fi
-  done
-}
 
-echo "0.0.0.0/0       dns-irc.netgamers.org       tcp     80" | helper_dns dns-irc.netgamers.org
+  TMPFILE=$(mktemp -p /dev/shm firewall-helper-dns-$(date -I).XXXXXXXXXX)
+  cat > $TMPFILE
+  replacement=$(cat $TMPFILE)
+
+  if echo $replacement | grep '\b'dns- > /dev/null; then
+    dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
+    hostname=${dns_replace#dns-}
+    host -t a $hostname | while read name dummy ip; do
+      replacement=$( sed s/$dns_replace/$ip/ < $TMPFILE )
+      if echo $replacement | grep '\b'dns- > /dev/null; then
+        echo $replacement | helper_dns
+      else
+        echo $replacement
+      fi
+    done
+  else
+    echo $replacement
+  fi
+  rm $TMPFILE
+}
