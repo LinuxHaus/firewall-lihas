73e833675cda9e4a64a5fdb1dd5b5bc00b46766b
Adrian Reyer <are@lihas.de>
2008-08-22 07:50:56 +0000

    *** empty log message ***

--- lib/helper-group.sh
+++ lib/helper-group.sh
@@ -4,7 +4,7 @@ helper_hostgroup () {
   cat | while read replacement; do
     if echo $replacement | grep '\b'hostgroup- > /dev/null; then
       hostgroup_replace=$(echo $replacement | sed 's/.*hostgroup-/hostgroup-/; s/hostgroup-\([^ \t]*\)[ \t].*/hostgroup-\1/')
-      cat groups/$hostgroup_replace | sed '/^[ \t]*$/d; /^#/d' |
+      cat groups/$hostgroup_replace | sed '/^[ \t]*$/d; /^#/d; s/\//\\\/g' |
       while read name; do
         repl=$( echo $replacement | sed s/$hostgroup_replace/$name/ )
         if echo $repl | grep '\b'hostgroup- > /dev/null; then
@@ -25,7 +25,7 @@ helper_portgroup () {
       repproto=$(echo $replacement | awk '{print $3}')
       portgroup_replace=$(echo $replacement | sed 's/.*portgroup-/portgroup-/; s/portgroup-\([^ \t]*\)[ \t].*/portgroup-\1/')
       if [ "$repproto" == "any" ]; then
-        cat groups/$portgroup_replace | sed '/^[ \t]*$/d; /^#/d' |
+        cat groups/$portgroup_replace | sed '/^[ \t]*$/d; /^#/d; s/\//\\\/g' |
         while read proto port; do
           repl=$( echo $replacement | sed "s/$portgroup_replace/$port/g; s/any/$proto/" )
           if echo $repl | grep '\b'portgroup- > /dev/null; then
@@ -35,7 +35,7 @@ helper_portgroup () {
           fi
         done
       else
-        cat groups/$portgroup_replace | sed '/^[ \t]*$/d; /^#/d' | awk '$1 ~ /^'$repproto'/' |
+        cat groups/$portgroup_replace | sed '/^[ \t]*$/d; /^#/d; s/\//\\\/g' | awk '$1 ~ /^'$repproto'/' |
         while read proto port; do
           repl=$( echo $replacement | sed "s/$portgroup_replace/$port/g;" )
           if echo $repl | grep '\b'portgroup- > /dev/null; then
