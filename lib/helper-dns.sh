f22d852a1dbff2569fc4ac2a27951e294b1a128c
Adrian Reyer <are@lihas.de>
2010-09-13 11:49:46 +0000

    *** empty log message ***

--- lib/helper-dns.sh
+++ lib/helper-dns.sh
@@ -6,12 +6,16 @@ helper_dns () {
     if echo $replacement | grep '\b'dns- > /dev/null; then
       dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
       hostname=${dns_replace#dns-}
-      host -t a $hostname | while read name dummy ip; do
-        repl=$( echo $replacement | sed s/$dns_replace/$ip/ )
-        if echo $repl | grep '\b'dns- > /dev/null; then
-          echo $repl | helper_dns
+      host -t a $hostname | sed 's/^.*[ \t]\+//' | while read ip; do
+        if echo $ip | egrep -q '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'; then
+          repl=$( echo $replacement | sed s/$dns_replace/$ip/ )
+          if echo $repl | grep '\b'dns- > /dev/null; then
+            echo $repl | helper_dns
+          else
+            echo $repl
+          fi
         else
-          echo $repl
+          echo >&2 "$hostname DNS lookup timed out"
         fi
       done
     else
