b562789325a9c25e826596afdf51661586c42e22
Adrian Reyer <are@lihas.de>
2010-11-24 17:51:59 +0000

    *** empty log message ***

--- lib/helper-dns.sh
+++ lib/helper-dns.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+
+
 helper_dns () {
 
   cat | while read replacement; do
     if echo $replacement | grep '\b'dns- > /dev/null; then
       dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
       hostname=${dns_replace#dns-}
-      host -t a $hostname | sed 's/^.*[ \t]\+//' | while read ip; do
+      ( host -t a $hostname | sed 's/^.*[ \t]\+//'
+      ) | while read ip; do
         if echo $ip | egrep -q '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'; then
           repl=$( echo $replacement | sed s/$dns_replace/$ip/ )
           if echo $repl | grep '\b'dns- > /dev/null; then
