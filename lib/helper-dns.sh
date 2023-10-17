580267f6bdc22770483334dc155a67fa05691ab8
Adrian Reyer <are@lihas.de>
2023-10-17 23:06:34 +0200

    minor: fix ident

--- lib/helper-dns.sh
+++ lib/helper-dns.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
-
 helper_dns () {
-
   cat | while read replacement; do
     if echo $replacement | grep '\b'dns- > /dev/null; then
       dns_replace=$(echo $replacement | sed 's/.*dns-/dns-/; s/dns-\([^ \t]*\)[ \t].*/dns-\1/')
@@ -12,16 +9,16 @@ helper_dns () {
         echo "$replacement"
       else
         host -t a $hostname | sed 's/^.*[ \t]\+//' | while read ip; do
-        if echo $ip | egrep -q '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'; then
-          repl=$( echo $replacement | sed s/$dns_replace/$ip/ )
-          if echo $repl | grep '\b'dns- > /dev/null; then
-            echo $repl | helper_dns
+          if echo $ip | egrep -q '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'; then
+            repl=$( echo $replacement | sed s/$dns_replace/$ip/ )
+            if echo $repl | grep '\b'dns- > /dev/null; then
+              echo $repl | helper_dns
+            else
+              echo $repl
+            fi
           else
-            echo $repl
+            echo >&2 "$hostname DNS lookup timed out"
           fi
-        else
-          echo >&2 "$hostname DNS lookup timed out"
-        fi
         done
       fi
     else
